b1c553bf8fa9a6fda4fc5c085fd9c5b803142d41
luiz-jolo <luizfelipe.jolo@gmail.com>
2024-03-01 14:26:46 -0400

    Starting project

---
+++ src/main/java/jolo/baikes/BaikesApplication.java
@@ -0,0 +1,13 @@
+package jolo.baikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jolo/baikes/BaikesApplicationTests.java
@@ -0,0 +1,13 @@
+package jolo.baikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
